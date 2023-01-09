7613aedf78fa4c3887d8119e35e2b89b1053f30f
KanishakVaidya <kanishakvaidya@gmail.com>
2023-01-10 02:35:23 +0530

    changed lang to take empty values and always return background color

--- .config/my_scripts/lang.sh
+++ .config/my_scripts/lang.sh
@@ -8,8 +8,13 @@ toggle_lang () {
 	fi
 }
 
+if [ -z ${BLOCK_BUTTON+x} ]; then
+    BLOCK_BUTTON=$1
+fi
+
 case $BLOCK_BUTTON in
 	1) toggle_lang ;;
+    *) ;;
 esac
 
 red=$(awk '/color1:/ {print $2}' ~/.Xresources)
@@ -19,6 +24,7 @@ echo "  $(setxkbmap -query | awk '/layout/{print $2}') "
 echo
 if [[ $(xset q | awk '/Caps Lock/{print $4}') == 'off' ]]; then
 	echo $red
+	echo $background
 else
 	echo $background
 	echo $red
