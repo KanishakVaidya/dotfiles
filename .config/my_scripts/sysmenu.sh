7c7fac4ed73e6b45fa8531e8568a52c72d0529be
KanishakVaidya <kanishakvaidya@gmail.com>
2023-01-22 18:39:46 +0530

    Added hover select in rofi and tweaked black

--- .config/my_scripts/sysmenu.sh
+++ .config/my_scripts/sysmenu.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-option=$(echo -e "Lock\nLogout\nShutDown\nReboot" | rofi -dmenu -p "Power Menu" -selected-row 0)
+option=$(echo -e "Lock\nLogout\nShutDown\nReboot" | rofi -dmenu -p "Power Menu" \
+    -selected-row 0 \
+    -theme my-theme \
+    -theme-str 'window {width: 15%;}'\
+    -location 3\
+    -fixed-num-lines 5 \
+    -hover-select \
+    -me-select-entry '' \
+    -me-accept-entry MousePrimary \
+    -xoffset -30 -yoffset 40)
 
 case $option in
-	Lock) i3lock -t -i ~/pic/lock.png ;;
-	Logout) i3-msg exit && logout ;;
-	ShutDown) shutdown now ;;
+	Lock) i3lock -c 111111 ;;
+	Logout) i3-msg exit || bspc quit ;;
+	ShutDown) poweroff ;;
 	Reboot) reboot ;;
 esac
